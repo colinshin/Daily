55c17b118380f143ac6039b2bfa9c55375c43899
idart <shenbau@163.com>
2020-06-25 11:01:00 +0800

    feature 完善导出功能 :smiley:

--- paper/admin.py
+++ paper/admin.py
@@ -1,4 +1,5 @@
 import time
+from io import BytesIO
 
 import xlwt
 from django.contrib import admin
@@ -97,91 +98,146 @@ class RecordAdmin(admin.ModelAdmin):
             else:
                 return qs.filter(creator=request.user)
 
+    def save_model(self, request, obj, form, change):
+        if change:
+            pass
+        else:
+            userprofile = UserProfile.objects.filter(user_id=request.user.id).first()
+            obj.name = request.user.username
+            obj.employee_no = userprofile.employee_no
+            obj.department = userprofile.depar
+            obj.group = userprofile.group
+            obj.creator = request.user
+            obj.company = userprofile.company
+            obj.project = userprofile.project
+            obj.region = userprofile.region
+            obj.direction = userprofile.direction
+        super(RecordAdmin, self).save_model(request, obj, form, change)  # this is daily file
+
     def export_excel(self, request, queryset):
         str_time = time.strftime("%Y%m%d", time.localtime())
-        # field_names = [field.name for field in Record._meta.fields]  # 模型所有字段名
-        field_names = ['name', 'employee_no', 'group', 'task_progress', 'tomorrow_task', 'direction',
-                       'region', 'company', 'project']  # 模型所有字段名  'pub_date'
-        field_names2 = ['task_progress', 'tomorrow_task']  # 模型所有字段名
-        header_style = xlwt.XFStyle()  # 初始化来样式源百
-        font2 = xlwt.Font()  # 为样式创建字体度
-        font2.colour_index = 0xFF00
-        font2.bold = True
-        font2.height = 0x0160
-        header_style.font = font2  # 设定样式
-        al2 = xlwt.Alignment()
-        al2.horz = 0x02  # 设置水平居中
-        al2.vert = 0x01  # 设置垂直居中
-        header_style.alignment = al2
 
+        filename = urlquote('daily report')
+        response = HttpResponse(content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachment;filename=%s' % filename + str(str_time) + '.xls'
 
-        wb = xlwt.Workbook(encoding='utf-8')  # 开始创建excel
-        wb_sheet = wb.add_sheet(str(str_time), cell_overwrite_ok=True)  # excel中的表名
-        wid1 = wb_sheet.col(0)
-        wid1.width = 70 * 70
-        wid2 = wb_sheet.col(1)
-        wid2.width = 70 * 70
-        wid3 = wb_sheet.col(2)
-        wid3.width = 70 * 70
-        wid4 = wb_sheet.col(3)
-        wid4.width = 120 * 120
-        wid5 = wb_sheet.col(4)
-        wid5.width = 120 * 120
-        wid5 = wb_sheet.col(5)
-        wid5.width = 70 * 70
-        wb_sheet.write(0, 0, '姓名', header_style)
-        wb_sheet.write(0, 1, '工号', header_style)
-        wb_sheet.write(0, 2, '方向', header_style)
-        wb_sheet.write(0, 3, '任务及进度', header_style)
-        wb_sheet.write(0, 4, '明日计划', header_style)
-        wb_sheet.write(0, 5, '业务组', header_style)
-        style = xlwt.XFStyle()  # 初始化来样式源百
-        font = xlwt.Font()  # 为样式创建字体度
-        style.font = font  # 设定样式
+        wb = xlwt.Workbook(encoding='utf8')
+
+        sheet = wb.add_sheet(str(str_time), cell_overwrite_ok=True)
+        sheet = self._set_header(sheet)
+        sheet = self._set_with(sheet)
+
+        field_names = ['name', 'employee_no', 'group', 'task_progress',
+                       'overall', 'tomorrow_task', 'direction',
+                       'project', 'region', 'company']
+        shot_names = ['task_progress', 'overall', 'tomorrow_task']
+        direction_names = Direction.objects.filter().all().values_list("name",
+                                                                       flat=True)
+
+        data_list, names = self._organize_data(direction_names, queryset,
+                                               field_names, shot_names)
+        self._write_excel(data_list, sheet, field_names, shot_names, names)
+
+        output = BytesIO()
+        wb.save(output)
+
+        output.seek(0)
+        response.write(output.getvalue())
+        return response
+
+    export_excel.short_description = '导出Excel'
+
+    def _write_excel(self, result_list, sheet, field_names, short_names, names):
+        """
+
+        :return:
+        :param result_list:
+        :param sheet:
+        :param field_names:
+        :param shot_names:
+        :param names:
+        :return:
+        """
+        style = xlwt.XFStyle()
+        font = xlwt.Font()
+        style.font = font
         al = xlwt.Alignment()
-        al.horz = 0x02  # 设置水平居中
-        al.vert = 0x01  # 设置垂直居中
+        al.horz = 0x02
+        al.vert = 0x01
         style.alignment = al
-        style.alignment.wrap = 1  # 自动知换行
+        style.alignment.wrap = 1
 
         borders = xlwt.Borders()  # Create borders
-        borders.left = xlwt.Borders.MEDIUM  # 添加边框-虚线边框
-        borders.right = xlwt.Borders.MEDIUM  # 添加边框-虚线边框
-        borders.top = xlwt.Borders.MEDIUM  # 添加边框-虚线边框
-        borders.bottom = xlwt.Borders.MEDIUM  # 添加边框-虚线边框
-        '''
-        May be: NO_LINE, THIN, MEDIUM, DASHED, DOTTED, THICK, DOUBLE, 
-        HAIR, MEDIUM_DASHED, THIN_DASH_DOTTED, MEDIUM_DASH_DOTTED, 
-        THIN_DASH_DOT_DOTTED, MEDIUM_DASH_DOT_DOTTED, SLANTED_MEDIUM_DASH_DOTTED, 
-        or 0x00 through 0x0D.
-        '''
-        borders.left_colour = 0x90  # 边框上色
-        borders.right_colour = 0x90
-        borders.top_colour = 0x90
-        borders.bottom_colour = 0x90
+        borders.left = xlwt.Borders.MEDIUM
+        borders.right = xlwt.Borders.MEDIUM
+        borders.top = xlwt.Borders.MEDIUM
+        borders.bottom = xlwt.Borders.MEDIUM
 
-        style.borders = borders
-        header_style.borders = borders
+        borders.left_colour = 0x3A
+        borders.right_colour = 0x3A
+        borders.top_colour = 0x3A
+        borders.bottom_colour = 0x3A
 
-        style1 = xlwt.XFStyle()  # 初始化来样式源百
-        style1.font = font  # 设定样式
+        style1 = xlwt.XFStyle()
+        style1.font = font
         al1 = xlwt.Alignment()
-        al1.vert = 0x01  # 设置垂直居中
+        al1.vert = 0x01
+        al1.horz = 0x01
         style1.alignment = al1
-        style1.alignment.wrap = 1  # 自动知换行
+        style1.alignment.wrap = 1
         style1.borders = borders
         tall_style = xlwt.easyxf('font:height 800')  # 36pt
+
+        style.borders = borders
+        merge_line = 0
         line = 0
-        group_names = Group.objects.filter().all().values_list("name", flat=True)
+        group_name = names[0]
+        project_name = names[1]
+        company_name = names[2]
+        for data_list in result_list:
+            direct = None
+            start_line = line + 1
+            for data_dict in data_list:
+                direct = data_dict.get('direction')
+                column = 0
+                line = line + 1
+                this_row = sheet.row(line)
+                this_row.set_style(tall_style)
+                for field in field_names:
+                    each_cell = data_dict.get(field)
+                    if field not in short_names:
+                        sheet.write(line, column, each_cell, style)
+                    else:
+                        sheet.write(line, column, each_cell, style1)
+
+    def _organize_data(self, direction_names, queryset, field_names, shot_names):
+
+        """
+
+        :param direction_names:
+        :param queryset:
+        :param field_names:
+        :param shot_names:
+        :return:
+        """
+        group_name = None
+        project_name = None
+        company_name = None
         result_list = []
-        for group_name in group_names:
+        for direct_name in direction_names:
             data_list = []
             for obj in queryset:
-                if group_name == getattr(obj, 'group'):
+                if group_name is None or project_name is None or \
+                        company_name is None:
+                    group_name = obj.group.name
+                    project_name = obj.project
+                    company_name = obj.company
+                direct = getattr(obj, 'direction').name
+                if direct_name == direct:
                     data_dict = dict()
                     for field in field_names:
                         each_cell = getattr(obj, field)
-                        if field not in field_names2:
+                        if field not in shot_names:
                             if field in ['group', 'direction']:
                                 data_dict.update({field: each_cell.name})
                             else:
@@ -189,45 +245,76 @@ class RecordAdmin(admin.ModelAdmin):
                         else:
                             data_dict.update({field: each_cell})
                     data_list.append(data_dict)
-            result_list.append(data_list)
-        for data_list in result_list:
-            for data_dict in data_list:
-                column = 0
-                line = line + 1
-                this_row = wb_sheet.row(line)
-                this_row.set_style(tall_style)
-                for field in field_names:
-                    each_cell = data_dict.get(field)
-                    if field not in field_names2:
-                        if field in ['group', 'direction']:
-                            wb_sheet.write(line, column, each_cell, style)
-                        else:
-                            wb_sheet.write(line, column, each_cell, style)
-                    else:
-                        wb_sheet.write(line, column, each_cell, style1)
-                    column = column + 1
-        response = HttpResponse(content_type='application/vnd.ms-excel')
-        filename = urlquote('工作日报')  # 导出数据 中文文件名称 变成下载.xls
-        response['Content-Disposition'] = 'attachment;filename=%s' % filename + str(str_time) + '.xlsx'
-        wb.save(response)
-        return response
+            if len(data_list) > 0:
+                result_list.append(data_list)
+        return result_list, (group_name, project_name, company_name)
 
-    export_excel.short_description = '导出Excel'
+    def _set_header(self, sheet):
+        """
 
-    def save_model(self, request, obj, form, change):
-        if change:  # 更改的时候
-            pass
-        else:  # 新增的时候
-            userprofile = UserProfile.objects.filter(user_id=request.user.id).first()
-            obj.name = request.user.username
-            obj.employee_no = userprofile.employee_no
-            obj.department = userprofile.depart
-            obj.group = userprofile.group
-            obj.creator = request.user
-            obj.company = userprofile.company
-            obj.project = userprofile.project
-            obj.region = userprofile.region
-        super(RecordAdmin, self).save_model(request, obj, form, change)
+        :param sheet:
+        :return:
+        """
+
+        style_heading = xlwt.easyxf("""
+                        font:
+                            name Arial,
+                            colour_index black,
+                            bold on,
+                            height 0x0120;
+                        align:
+                            wrap off,
+                            vert center,
+                            horiz center;
+                        pattern:
+                            pattern solid,
+                            fore-colour yellow;
+                        borders:
+                            left THIN,
+                            right THIN,
+                            top THIN,
+                            bottom THIN;
+                        """)
+
+        sheet.write(0, 0, 'name', style_heading)
+        sheet.write(0, 1, 'employee_no', style_heading)
+        sheet.write(0, 2, 'yewuzu', style_heading)
+        sheet.write(0, 3, 'jinrimubiao', style_heading)
+        sheet.write(0, 4, 'zongtijindu', style_heading)
+        sheet.write(0, 5, 'mingrijihua', style_heading)
+        sheet.write(0, 6, 'yewuzifenzu', style_heading)
+        sheet.write(0, 7, 'suoshuxiangmu', style_heading)
+        sheet.write(0, 8, 'suozaidi', style_heading)
+        sheet.write(0, 9, 'suoshugongsi', style_heading)
+        return sheet
+
+    def _set_with(self, wb_sheet):
+        """
+
+        :param wb_sheet:
+        :return:
+        """
+        wid1 = wb_sheet.col(0)
+        wid1.width = 50 * 50
+        wid2 = wb_sheet.col(1)
+        wid2.width = 50 * 50
+        wid3 = wb_sheet.col(2)
+        wid3.width = 70 * 70
+        wid4 = wb_sheet.col(3)
+        wid4.width = 100 * 100
+        wid5 = wb_sheet.col(4)
+        wid5.width = 100 * 100
+        wid5 = wb_sheet.col(5)
+        wid5.width = 100 * 100
+        wid6 = wb_sheet.col(6)
+        wid6.width = 90 * 90
+        wid7 = wb_sheet.col(7)
+        wid7.width = 70 * 70
+        wid8 = wb_sheet.col(8)
+        wid8.width = 50 * 50
+        wid9 = wb_sheet.col(9)
+        wid9.width = 70 * 70
+        return wb_sheet
 
 
 @admin.register(Department)
